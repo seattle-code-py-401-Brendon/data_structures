a2e786412302092831a34b46184ba93fbcecc977
Brendon <brendonhampton1988@gmail.com>
2022-07-02 21:42:18 -0700

    added queeus to the animal shelter for each dog and cat

--- python/code_challenges/stack_queue_animal_shelter/stack_queue_animal_shelter.py
+++ python/code_challenges/stack_queue_animal_shelter/stack_queue_animal_shelter.py
@@ -1,10 +1,33 @@
+from python.code_challenges.data_structures.stacks_and_queues.queue import Queue
+
+
 class AnimalShelter:
-    pass
+    def __init__(self):
+        self.dog_shelter = Queue()
+        self.cat_shelter = Queue()
+
+    def enqueue(self, animal):
+        if animal == 'Dog':
+            dog = Dog()
+            self.dog_shelter.enqueue(dog.value)
+        if animal == 'Cat':
+            cat = Cat()
+            self.cat_shelter.enqueue(cat.value)
+        else:
+            return 'please dont leave an animal, they need lovies!!'
 
 
 class Dog:
-    pass
+    def __init__(self):
+        self.value = 'Dog'
+        self.next = None
 
 
 class Cat:
     pass
+
+
+if __name__ == '__main__':
+    animal_shelter = AnimalShelter()
+    animal_shelter.enqueue('Dog')
+    print(animal_shelter.dog_shelter.front.value)
