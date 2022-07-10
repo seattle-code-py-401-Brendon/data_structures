65ce254bf5f2e0db1f4a54c9221121019892f933
Brendon <brendonhampton1988@gmail.com>
2022-07-10 00:46:08 -0700

    finished add method for BST

--- python/code_challenges/data_structures/binary_search_tree.py
+++ python/code_challenges/data_structures/binary_search_tree.py
@@ -1,4 +1,4 @@
-from python.code_challenges.data_structures.binary_tree.binary_tree import BinaryTree
+from python.code_challenges.data_structures.binary_tree.binary_tree import BinaryTree, Node
 
 
 class BinarySearchTree(BinaryTree):
@@ -8,8 +8,30 @@ class BinarySearchTree(BinaryTree):
 
     def __init__(self):
         # initialization here
-        pass
+        self.binary_tree = BinaryTree()
 
-    def some_method(self):
+    def add(self, value):
         # method body here
-        pass
+        if self.binary_tree.root is None:
+            self.binary_tree.root = Node(value)
+        if self.binary_tree.root.value > value:
+            if self.binary_tree.root.left is None:
+                self.binary_tree.root.left = Node(value)
+            else:
+                self.binary_tree.root = self.binary_tree.root.left
+                return self.insert(value)
+        elif self.binary_tree.root.value < value:
+            if self.binary_tree.root.right is None:
+                self.binary_tree.root.right = Node(value)
+            else:
+                self.binary_tree.root = self.binary_tree.root.right
+                return self.insert(value)
+
+
+if __name__ == '__main__':
+    tree = BinarySearchTree()
+    tree.add(5)
+    tree.add(2)
+    tree.add(6)
+    print(tree.binary_tree.root.left.value)
+    print(tree.binary_tree.root.right.value)
