ff74043fd867a14e76f594109b217233c1123c52
Brendon <brendonhampton1988@gmail.com>
2022-07-10 01:29:12 -0700

    undoing my last chamges, my deep root test doesnt pass

--- python/code_challenges/data_structures/binary_search_tree.py
+++ python/code_challenges/data_structures/binary_search_tree.py
@@ -8,30 +8,31 @@ class BinarySearchTree(BinaryTree):
 
     def __init__(self):
         # initialization here
-        self.binary_tree = BinaryTree()
+        self.root = None
 
     def add(self, value):
         # method body here
-        if self.binary_tree.root is None:
-            self.binary_tree.root = Node(value)
-        if self.binary_tree.root.value > value:
-            if self.binary_tree.root.left is None:
-                self.binary_tree.root.left = Node(value)
+        if self.root is None:
+            self.root = Node(value)
+        elif self.root.value > value:
+            if self.root.left is None:
+                self.root.left = Node(value)
             else:
-                self.binary_tree.root = self.binary_tree.root.left
-                return self.insert(value)
-        elif self.binary_tree.root.value < value:
-            if self.binary_tree.root.right is None:
-                self.binary_tree.root.right = Node(value)
+                self.root = self.root.left
+                return self.add(value)
+        elif self.root.value < value:
+            if self.root.right is None:
+                self.root.right = Node(value)
             else:
-                self.binary_tree.root = self.binary_tree.root.right
-                return self.insert(value)
+                self.root = self.root.right
+                return self.add(value)
 
 
 if __name__ == '__main__':
     tree = BinarySearchTree()
     tree.add(5)
+    tree.add(4)
+    tree.add(3)
     tree.add(2)
-    tree.add(6)
-    print(tree.binary_tree.root.left.value)
-    print(tree.binary_tree.root.right.value)
+    print(tree.root.value)
+
