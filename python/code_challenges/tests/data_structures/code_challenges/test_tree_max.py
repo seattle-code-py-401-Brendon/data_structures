d037da5985ef74f7288574201293f7d9ace64bf7
Brendon <brendonhampton1988@gmail.com>
2022-07-12 23:03:02 -0700

    added extra tests

--- python/code_challenges/tests/data_structures/code_challenges/test_tree_max.py
+++ python/code_challenges/tests/data_structures/code_challenges/test_tree_max.py
@@ -22,3 +22,32 @@ def test_max_val():
     expected = 30
 
     assert actual == expected
+
+
+# @pytest.mark.skip('TODO')
+def test_max_value_deep_tree():
+    tree = BinaryTree()
+    tree.root = Node(10)
+    tree.root.left = Node(12)
+    tree.root.left.right = Node(145)
+    tree.root.right = Node(113)
+    tree.root.right.left = Node(2)
+
+    actual = tree.find_maximum_value()
+    expected = 145
+
+    assert actual == expected
+
+
+# @pytest.mark.skip('TODO')
+def test_value_greater_than():
+    tree = BinaryTree()
+    tree.root = Node(10)
+    tree.root.left = Node(12)
+    tree.root.left.right = Node(145)
+    tree.root.right = Node(113)
+    tree.root.right.left = Node(2)
+
+    actual = tree.find_maximum_value()
+
+    assert actual > 50
