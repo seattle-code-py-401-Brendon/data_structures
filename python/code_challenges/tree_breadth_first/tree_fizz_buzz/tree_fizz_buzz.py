c3e4e1ba5a450891a0e0256770c5e26922042edf
Brendon <brendonhampton1988@gmail.com>
2022-07-19 00:22:19 -0700

    got all three tests to pass, added deepcopy

--- python/code_challenges/tree_breadth_first/tree_fizz_buzz/tree_fizz_buzz.py
+++ python/code_challenges/tree_breadth_first/tree_fizz_buzz/tree_fizz_buzz.py
@@ -1,15 +1,18 @@
+from python.code_challenges.data_structures.kary_tree import KaryTree, Node
+import copy
+
 def fizz_buzz_tree(kary_tree):
     if kary_tree is None:
         return 'empty tree'
-    new_tree = kary_tree
+    new_tree = copy.deepcopy(kary_tree)
 
     def tree_traverse(root):
         if root.value % 3 == 0 and root.value % 5 == 0:
-            root.value = 'FizzBuzz'
+            root.value = "FizzBuzz"
         elif root.value % 3 == 0:
-            root.value = 'Fizz'
+            root.value = "Fizz"
         elif root.value % 5 == 0:
-            root.value = 'Buzz'
+            root.value = "Buzz"
         elif root.value % 3 != 0 or root.value % 5 != 0:
             root.value = str(root.value)
         for child in root.children:
