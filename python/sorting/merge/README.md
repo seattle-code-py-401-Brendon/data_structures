a7aa1ddfd518ccf15b44129e72fa75cb6edb25ad
BrendonLH <Brendonhampton1988@gmail.com>
2022-08-11 21:58:58 -0700

    added png file uncompleted, added some code, havent finished tests

--- python/sorting/merge/README.md
+++ python/sorting/merge/README.md
@@ -1,1 +1,34 @@
 # Blog Notes: Merge Sort
+
+# Challenge Summary
+<!-- Description of the challenge -->
+Create a Blog that Walks through Merge sort
+
+## Links and Resources
+<!-- Embedded whiteboard image -->
+![WhiteBoard](merge_sort.png)
+
+### Link To Code
+<!-- Link to code solution file -->
+[Solution](merge_sort.py)
+
+### Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+Time Complexity:
+Space Complexity:
+
+### Solution
+<!-- Show how to run your code, and examples of it in action -->
+-
+
+### Tests
+<!-- test names and what they test for -->
+1. test one:
+    - 
+2. test two:
+    -
+3. test three:
+    - 
+4. test four:
+    - 
+
---
+++ python/sorting/merge/merge_sort.py
@@ -0,0 +1,40 @@
+def merge_sort(arr):
+    n = len(arr)
+
+    if n > 1:
+        mid = n // 2
+        left = arr[0: mid]
+        right = arr[mid: n]
+
+        merge_sort(left)
+
+        merge_sort(right)
+
+        merge(left, right, arr)
+
+
+def merge(left, right, arr):
+    i = 0
+    j = 0
+    k = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            arr[k] = left[i]
+            i = i + 1
+        else:
+            arr[k] = right[j]
+            j = j + 1
+
+        k = k + 1
+
+    if i == len(left):
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+    else:
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
